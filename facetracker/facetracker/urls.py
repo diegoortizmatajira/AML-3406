d9d54c1e4cc93d4b2389bbcae3b3bfae03c2a56e
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-07-20 21:41:10 -0400

    cleaning

--- facetracker/facetracker/urls.py
+++ facetracker/facetracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('emotions/', include('emotions.urls'))
+    path('detection/', include('detection.urls'))
 ]
