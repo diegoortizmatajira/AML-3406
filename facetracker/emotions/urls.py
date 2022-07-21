3397c5238934f0187309aec26f65ffbb292a89c4
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-07-20 21:23:38 -0400

    Cleaning

--- facetracker/emotions/urls.py
+++ facetracker/emotions/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     #display both cameras
     path('', views.index, name='index'),
     #access the laptop camera
-    path('get_emotions', views.get_emotions, name='get_emotions'),
-    path('video_feed', views.video_feed, name='video_feed'),
     path('results', views.results, name='results'),
     path('camera_endpoint', views.camera_endpoint, name='camera_endpoint'),
 ]
